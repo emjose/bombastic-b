19c8d9e53a7c905313206dc7191b96237066f764
emjose <emjose@gmail.com>
2020-11-08 13:17:12 -0500

    reset pk sequence gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# CartJoiner.destroy_all
+# OrderJoiner.destroy_all
+# Review.destroy_all
+# Cart.destroy_all
+# Order.destroy_all
+# Item.destroy_all
+# User.destroy_all
+
+
+# CartJoiner.reset_pk_sequence
+# OrderJoiner.reset_pk_sequence
+# Review.reset_pk_sequence
+# Cart.reset_pk_sequence
+# Order.reset_pk_sequence
+# Item.reset_pk_sequence
+# User.reset_pk_sequence
+
+# t.string "title"
+# t.string "description"
+# t.integer "quantity"
+# t.integer "price"
+# t.string "color"
+# t.string "colorb"
+# t.string "height"
+# t.string "category"
+# t.string "image"
+# t.string "imageb"
+# t.string "imagec"
