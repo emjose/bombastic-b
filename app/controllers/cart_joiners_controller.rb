c258c47fa2877ce91984ed976850085b3033aae6
emjose <emjose@gmail.com>
2020-11-08 22:52:46 -0500

    seed data update

--- app/controllers/cart_joiners_controller.rb
+++ app/controllers/cart_joiners_controller.rb
@@ -12,7 +12,7 @@ class CartJoinersController < ApplicationController
 
     def create
         added_item = Item.find(params[:item_id])
-        new_item_quantity = added_item.quantity - 1
+        new_item_quantity = added_item.quantity + 1
         added_item.update(quantity: new_item_quantity)
         create_cart_joiner = cart_joiner_params.merge({cart_id: logged_user.cart.id})
         cart_joiner = CartJoiner.create(create_cart_joiner)
@@ -23,7 +23,7 @@ class CartJoinersController < ApplicationController
         @cart_joiner = CartJoiner.find(params[:id])
         @removed_id = @cart_joiner.item_id
         @removed_item = Item.find(@removed_id)
-        @new_item_quantity = @removed_item.quantity + 1
+        @new_item_quantity = @removed_item.quantity - 1
         @removed_item.update(quantity: @new_item_quantity)
         @cart_joiner.destroy
         render json: {message: "Item has been deleted", cart_joiner: @cart_joiner, item: @removed_item}
